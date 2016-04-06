b701fb6f6e4df1949fe04843a5d9cd502d0600ad
kbfl-campaign <kbfl-campaign@kbfl.org>
2016-04-06 20:39:33 +0800

    release v1.1
    Updated theme for app.html, updated infographic, minor tweak.

--- assets/index.js
+++ assets/index.js
@@ -1,17 +1,25 @@
-window.setInterval(function () {
-  var t = (Math.floor(Date.now()/1000)-1459353600)/86400;
-  var formGenerated = (
-    6500+(
-      Math.floor(
-        288/2*(
-          t-(
-            (Math.sin(2*Math.PI*t))/(2*Math.PI)
+$(function () {
+  window.setInterval(function () {
+    var t = (Math.floor(Date.now()/1000)-1459353600)/86400;
+    var formGenerated = (
+      6500+(
+        Math.floor(
+          288/2*(
+            t-(
+              (Math.sin(2*Math.PI*t))/(2*Math.PI)
+            )
           )
         )
       )
-    )
-  );
-  var seatFormRatio =  Math.round(formGenerated/47300*100*10)/10 ;
-  $('.formGenerated').text(formGenerated.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
-  $('.seatFormRatio').text(seatFormRatio.toString()+"%");
-}, 1000);
+    );
+    var seatFormRatio =  Math.round(formGenerated/47300*100*10)/10 ;
+    $('.formGenerated').text(formGenerated.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    $('.seatFormRatio').text(seatFormRatio.toString()+"%");
+  }, 1000);
+
+  var parent = $(".partner-container");
+  var divs = parent.children();
+  while (divs.length) {
+      parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+  }
+});
